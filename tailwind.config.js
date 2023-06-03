2aaa6bdc48fcd9eb986304a20739bb440cd53c02
Johncarlos Lunardini <john-carlos.lunardini@mikestechshop.com>
2023-06-02 21:36:25 -0400

    reworked profile page

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ module.exports = {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
 				"gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+
+			colors: {
+				white1: "#F5F5F5",
+				white2: "#FAFAFA",
+				gray1: "#737373",
+			},
+			fontFamily: {
+				sans: ["var(--font-inter)"],
+				mono: ["var(--font-roboto-mono)"],
+			},
 		},
 	},
 	plugins: [],
