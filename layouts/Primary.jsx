4825fad3d422534e06f5ca0ae0cf11c938ac82a2
Johncarlos Lunardini <john-carlos.lunardini@mikestechshop.com>
2023-06-08 17:15:52 -0400

    resolve overflow x on mobile

--- layouts/Primary.jsx
+++ layouts/Primary.jsx
@@ -2,106 +2,103 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Primary({ children, nav }) {
-	const router = useRouter();
-	return (
-		<div className="flex flex-col w-full bg-white1 min-h-[calc(100dvh)] overflow-x-auto">
-			{nav && (
-				<div className="bg-white2 shadow-sm h-auto">
-					<div className="max-w-full lg:max-w-[800px] px-4 lg:mx-auto flex flex-row justify-center md:justify-between  gap-4 lg:gap-8 py-4 ">
-						<Link
-							href="/my-reviews"
-							className={`${
-								router.pathname == "/my-reviews" ? "bg-white1 text-black" : "text-gray1"
-							} flex-1 md:flex-grow-0 text-center whitespace-nowrap px-4 py-2 hover:bg-white1 rounded-md transition-colors`}
-						>
-							My Reviews
-						</Link>
-						<Link
-							href="/the-feed"
-							className={`${
-								router.pathname == "/the-feed" ? "bg-white1 text-black" : "text-gray1"
-							} flex-1 md:flex-grow-0 text-center whitespace-nowrap px-4 py-2 hover:bg-white1 rounded-md transition-colors`}
-						>
-							The Feed
-						</Link>
-						<Link
-							href="/"
-							className={`${
-								router.pathname == "/" ? "bg-white1 text-black" : "text-gray1"
-							} flex-1 md:flex-grow-0 text-center whitespace-nowrap px-4 py-2 hover:bg-white1 rounded-md transition-colors`}
-						>
-							Profile
-						</Link>
-					</div>
-				</div>
-			)}
+  const router = useRouter();
+  return (
+    <div className="flex flex-col w-full bg-white1 min-h-[calc(100dvh)] overflow-x-auto">
+      {nav && (
+        <div className="bg-white2 shadow-sm h-auto">
+          <div className="max-w-full lg:max-w-[800px] px-4 lg:mx-auto flex flex-row justify-center md:justify-between  gap-4 lg:gap-8 py-4 ">
+            <Link
+              href="/my-reviews"
+              className={`${router.pathname == "/my-reviews" ? "bg-white1 text-black" : "text-gray1"
+                } flex-1 md:flex-grow-0 text-center whitespace-nowrap px-4 py-2 hover:bg-white1 rounded-md transition-colors`}
+            >
+              My Reviews
+            </Link>
+            <Link
+              href="/the-feed"
+              className={`${router.pathname == "/the-feed" ? "bg-white1 text-black" : "text-gray1"
+                } flex-1 md:flex-grow-0 text-center whitespace-nowrap px-4 py-2 hover:bg-white1 rounded-md transition-colors`}
+            >
+              The Feed
+            </Link>
+            <Link
+              href="/"
+              className={`${router.pathname == "/" ? "bg-white1 text-black" : "text-gray1"
+                } flex-1 md:flex-grow-0 text-center whitespace-nowrap px-4 py-2 hover:bg-white1 rounded-md transition-colors`}
+            >
+              Profile
+            </Link>
+          </div>
+        </div>
+      )}
 
-			<div className="py-12 lg:py-16 flex-grow px-4 mx-auto lg:w-[800px] flex flex-col h-full">
-				{children}
-			</div>
-			<div className="bg-white2 shadow-sm relative bottom-0">
-				<div className="max-w-full lg:max-w-[800px] px-4 lg:mx-auto flex flex-row justify-center  gap-4 lg:gap-8 py-4">
-					<div className="p-0 lg:px-5 flex flex-row items-center">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="fill-yellow-300 w-5 h-5 rotate-180"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"
-							/>
-						</svg>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="fill-yellow-300 w-5 h-5 rotate-180"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"
-							/>
-						</svg>
-						<p className="mx-3 font-sans text-gray1">ReviewBrah 2023</p>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="fill-yellow-300 w-5 h-5 rotate-180"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"
-							/>
-						</svg>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							strokeWidth={1.5}
-							stroke="currentColor"
-							className="fill-yellow-300 w-5 h-5 rotate-180"
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"
-							/>
-						</svg>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+      <div className="w-full py-12 lg:py-16 flex-grow px-4 mx-auto lg:w-[800px] flex flex-col h-full">
+        {children}
+      </div>
+      <div className="bg-white2 shadow-sm relative bottom-0">
+        <div className="max-w-full lg:max-w-[800px] px-4 lg:mx-auto flex flex-row justify-center  gap-4 lg:gap-8 py-4">
+          <div className="p-0 lg:px-5 flex flex-row items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="fill-yellow-300 w-5 h-5 rotate-180"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="fill-yellow-300 w-5 h-5 rotate-180"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"
+              />
+            </svg>
+            <p className="mx-3 font-sans text-gray1">ReviewBrah 2023</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="fill-yellow-300 w-5 h-5 rotate-180"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="fill-yellow-300 w-5 h-5 rotate-180"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M7.5 15h2.25m8.024-9.75c.011.05.028.1.052.148.591 1.2.924 2.55.924 3.977a8.96 8.96 0 01-.999 4.125m.023-8.25c-.076-.365.183-.75.575-.75h.908c.889 0 1.713.518 1.972 1.368.339 1.11.521 2.287.521 3.507 0 1.553-.295 3.036-.831 4.398C20.613 14.547 19.833 15 19 15h-1.053c-.472 0-.745-.556-.5-.96a8.95 8.95 0 00.303-.54m.023-8.25H16.48a4.5 4.5 0 01-1.423-.23l-3.114-1.04a4.5 4.5 0 00-1.423-.23H6.504c-.618 0-1.217.247-1.605.729A11.95 11.95 0 002.25 12c0 .434.023.863.068 1.285C2.427 14.306 3.346 15 4.372 15h3.126c.618 0 .991.724.725 1.282A7.471 7.471 0 007.5 19.5a2.25 2.25 0 002.25 2.25.75.75 0 00.75-.75v-.633c0-.573.11-1.14.322-1.672.304-.76.93-1.33 1.653-1.715a9.04 9.04 0 002.86-2.4c.498-.634 1.226-1.08 2.032-1.08h.384"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
