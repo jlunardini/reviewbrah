3631bec7b6b0ee946e12acf89e1ff4803caf72b3
Johncarlos Lunardini <john-carlos.lunardini@mikestechshop.com>
2023-06-08 02:38:36 -0400

    add key

--- pages/my-reviews.jsx
+++ pages/my-reviews.jsx
@@ -56,6 +56,7 @@ export default function MyReview({}) {
 						{feed &&
 							feed.map((item) => (
 								<ReviewCard
+									key={`review-${item.id}`}
 									review={item}
 									username={false}
 									category={true}
